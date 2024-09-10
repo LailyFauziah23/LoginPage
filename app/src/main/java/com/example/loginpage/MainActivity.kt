ed0732461ec057e3294dab32110ade2a5943be00
LailyFauziah23 <lailynuriyatulfauziah@mail.ugm.ac.id>
2024-09-10 20:33:03 +0700

    add toast

--- app/src/main/java/com/example/loginpage/MainActivity.kt
+++ app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.loginpage
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    }
+
+    // Method to handle the button click event
+    fun onSignUpClick(view: android.view.View) {
+        // Find the EditText fields
+        val emailField = findViewById<EditText>(R.id.email)
+        val fullNameField = findViewById<EditText>(R.id.fullname)
+        val usernameField = findViewById<EditText>(R.id.username)
+        val passwordField = findViewById<EditText>(R.id.password)
+
+        // Retrieve the text entered by the user
+        val email = emailField.text.toString()
+        val fullName = fullNameField.text.toString()
+        val username = usernameField.text.toString()
+        val password = passwordField.text.toString()
+
+        // Display a Toast with the user input
+        Toast.makeText(this, "Email: $email\nFullname: $fullName\nUsername: $username\nPassword: $password", Toast.LENGTH_LONG).show()
     }
 }
